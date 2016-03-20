cdedfa2309c751dc5cae5b93c66f1630a755beca
Suiken <Kevin.Suy@me.com>
2016-03-20 23:03:08 +0100

    execute sh test

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,19 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        try {
+            ProcessBuilder pb = new ProcessBuilder("tp2-app.sh", "h");
+            Process p = pb.start();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(p.getInputStream()));
+            String line = null;
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+        }catch(Exception e){
+            e.printStackTrace();
+        }
     }
 }
