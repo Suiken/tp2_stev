15d3423838cd626cb3c955fe54db71cdb69e02ad
vince <v.bathellier@hotmail.fr>
2016-03-20 18:46:25 -0400

    execut sh

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,7 @@ public class Main {
     public static void main(String[] args) throws IOException {
         try {
             Process p = Runtime.getRuntime().exec("/home/stev/tp2-app.sh -h");
+            p.getErrorStream();
             BufferedReader reader = new BufferedReader(new InputStreamReader(p.getInputStream()));
             String line = null;
             while ((line = reader.readLine()) != null) {
@@ -14,5 +15,11 @@ public class Main {
         }catch(Exception e){
             e.printStackTrace();
         }
+    }
+
+    public void extractArgument(String argument) {
+        String[] tokens = argument.split(",");
+        for (String t : tokens)
+            System.out.println(t);
     }
 }
