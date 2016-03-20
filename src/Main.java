7c53b2b61ea530984eaa3b786b54abbca86a5fcf
vince <v.bathellier@hotmail.fr>
2016-03-20 18:27:51 -0400

    execut sh

--- src/Main.java
+++ src/Main.java
@@ -4,15 +4,9 @@ import java.io.InputStream;
 public class Main {
     public static void main(String[] args) throws IOException {
 
-        try {
-            ProcessBuilder pb = new ProcessBuilder("/home/stev/tp2-app.sh");
-            Process p = pb.start();     // Start the process.
-            p.waitFor();                // Wait for the process to finish.
-            InputStream stderr = p.getErrorStream();
-            System.out.println(stderr);
-            System.out.println("Script executed successfully");
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        String[] cmd = { "bash", "-c", "/home/stev/tp2-app.sh -h" };
+        Process p = Runtime.getRuntime().exec(cmd);
+        InputStream stderr = p.getErrorStream();
+        System.out.println(stderr);
     }
 }
